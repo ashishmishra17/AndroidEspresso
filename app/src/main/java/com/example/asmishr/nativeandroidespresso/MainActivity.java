ec6870d77e09bae982975f0216ba16f831c51655
ashishmishra17 <ashish.june17@gmail.com>
2018-10-17 13:05:13 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/asmishr/nativeandroidespresso/MainActivity.java
+++ app/src/main/java/com/example/asmishr/nativeandroidespresso/MainActivity.java
@@ -17,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        int a=1,b=0;
+        int rsult = a/b;
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
